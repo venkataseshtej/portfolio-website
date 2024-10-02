29e474e524fc4458831bb8953f6e111e38cdb36d
Naga Sai <naga@wishup.co>
2024-10-02 21:48:19 +0530

    #customization skils 2

--- lib/data.ts
+++ lib/data.ts
@@ -85,23 +85,19 @@ export const projectsData = [
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
   "Python",
-  "Django",
-  "Framer Motion",
+  "C",
+  "C++",
+  "MATLAB",
+  "SQL",
+  "PyTorch",
+  "TensorFlow",
+  "vLLM",
+  "Hugging Face",
+  "LangChain",
+  "OpenAI",
+  "Scikit-Learn",
+  "SpaCy",
+  "Docker"
 ] as const;
+
