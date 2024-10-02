11c2d42e81819c0cdc4a338c0dd2dae461795791
Naga Sai <naga@wishup.co>
2024-10-02 22:11:26 +0530

    #c7

--- lib/data.ts
+++ lib/data.ts
@@ -35,29 +35,35 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Teaching Assistant ",
+    location: "Pennsylvania State University",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Oversaw all aspects of Programming Language Concepts course, including assignment creation, exam supervision, project ideation, and student support in Python, Scheme, OOP, alongside coordinating course logistics and communications",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "August 2024 - Present",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "NLP lab — Research Assistant ",
+    location: "Pennsylvania State University",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Developed and implemented Python scripts to simulate physical interactions using the PHYRE environment extracting object parameters such as position, velocity. Analyzed and extracted object parameters, ensuring data aligned with the ESPRIT dataset structure for machine learning tasks",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "July 2024 - Present",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+    title: "AI Research Assistant",
+    location: "Jefferson lab — Summer Research Internship",
+    description:"As part of my internship, I successfully applied machine learning techniques to analyze experimental data and create 3D models of proton distributions in nuclei. I also developed a Python-based data analysis pipeline and utilized advanced visualization techniques to contribute to advancements in modern physics and astrophysics research.",
+    icon: React.createElement(CgWorkAlt),
+    date: "May 2024 - present",
   },
+  {
+    title: "Machine Learning Engineer",
+    location: "Graphene AI Health Tech Pvt.Ltd",
+    description : "As a Machine Learning Engineer at Graphene AI Health Tech, I successfully implemented a relevance filter and knowledge graph, improving data extraction efficiency and reducing deployment time. These advancements contributed to the company's overall success in leveraging AI for healthcare applications.",
+    icon: React.createElement(CgWorkAlt),
+    date: "June 2022 - August 2023",
+  }
 ] as const;
 
 export const projectsData = [
@@ -98,6 +104,5 @@ export const skillsData = [
   "OpenAI",
   "Scikit-Learn",
   "SpaCy",
-  "Docker"
+  "Docker",
 ] as const;
-
