b4f00954a9512732fc155cc4ebd8d7b2c3243a53
Naga Sai <naga@wishup.co>
2024-10-02 21:53:29 +0530

    #customization 4.1

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs">
-        &copy; {currentYear} Ricardo. All rights reserved.
+        &copy; {currentYear} venkataseshtej.tech All rights reserved.
       </small>
       <p className="text-xs">
         Built with{" "}
