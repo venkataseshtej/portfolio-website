86fcbfddc53eec015d104cdd846d845c27af80f7
Naga Sai <naga@wishup.co>
2024-10-02 22:31:39 +0530

    #fixes

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,10 +63,11 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Sesh tej.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">Machine Learning Engineer</span> 
+        {/* with{" "} */}
+        {/* <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Next.js)</span>. */}
       </motion.h1>
 
       <motion.div
